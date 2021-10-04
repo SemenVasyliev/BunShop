ad70f05abd2d7f3ec21d4052250da460adc8b773
felixpincher <felixpincher@gmail.com>
2021-10-05 01:00:47 +0300

    first commit interfaces and models

---
+++ Data/Interfaces/IAllBuns.cs
@@ -0,0 +1,15 @@
+﻿using Shop2.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop2.Data.Interfaces
+{
+    interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; set; }
+        IEnumerable<Car> getFavCars { get; set; }
+        Car getObjectCar(int carId);
+    }
+}
---
+++ Data/mocks/MockBuns.cs
@@ -0,0 +1,29 @@
+﻿using Shop2.Data.Interfaces;
+using Shop2.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop2.Data.mocks
+{
+    public class MockCars : IAllCars
+    {
+        public IEnumerable<Car> Cars 
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car {name = "Tesla", shortDesc = "", longDesc = "", img = "", price = 45000, isFavourite = true, available = true}
+                };
+            }
+        }
+        public IEnumerable<Car> getFavCars { get ; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
